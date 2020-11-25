59bc8d8133a5fc39aab94fd4259976cefe862648
Nickael Vereza Bruzzi <qaznickael@hotmail.com>
2020-11-25 00:18:37 -0300

    add logic change filter

--- src/pages/Search/pages/Filter/index.js
+++ src/pages/Search/pages/Filter/index.js
@@ -1,13 +1,22 @@
-import React, { useState} from 'react';
-import { ScrollView } from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView} from 'react-native';
 
-import { 
-  Container, FilterView, Title, PriceView, PriceInput, 
-  ButtonContainer, ButtonView, ButtonText, ButtonSelected, ButtonSelectedText
- } from './styles';
+import {
+  Container,
+  FilterView,
+  Title,
+  PriceView,
+  PriceInput,
+  ButtonContainer,
+  ButtonView,
+  ButtonText,
+  ButtonSelected,
+  ButtonSelectedText,
+} from './styles';
 
 const Filter = () => {
-  const [to_sell, setTo_Sell] = useState(0);
+  const [to_sell, setTo_Sell] = useState(null);
+  const [toSellSelected, setToSellSelected] = useState(1);
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrince, setMaxPrice] = useState(0);
   const [type, setType] = useState(0);
@@ -18,61 +27,67 @@ const Filter = () => {
     <ScrollView style={{backgroundColor: '#fff'}}>
       <FilterView>
         <Title>Para: </Title>
-        
+
         <ButtonContainer>
-          <ButtonSelected>
-            <ButtonSelectedText>Ambos</ButtonSelectedText>
-          </ButtonSelected>
-          <ButtonView>
-            <ButtonText>Venda</ButtonText>
-          </ButtonView>
-          <ButtonView>
-            <ButtonText>Aluguel</ButtonText>
-          </ButtonView>
+          {toSellSelected === 1 ? (
+            <ButtonSelected>
+              <ButtonSelectedText>Ambos</ButtonSelectedText>
+            </ButtonSelected>
+          ) : (
+            <ButtonView
+              onPress={() => {
+                setToSellSelected(1);
+              }}>
+              <ButtonText>Ambos</ButtonText>
+            </ButtonView>
+          )}
+
+          {toSellSelected === 2 ? (
+            <ButtonSelected>
+              <ButtonSelectedText>Venda</ButtonSelectedText>
+            </ButtonSelected>
+          ) : (
+            <ButtonView
+              onPress={() => {
+                setToSellSelected(2);
+              }}>
+              <ButtonText>Venda</ButtonText>
+            </ButtonView>
+          )}
+          {toSellSelected === 3 ? (
+            <ButtonSelected>
+              <ButtonSelectedText>Aluguel</ButtonSelectedText>
+            </ButtonSelected>
+          ) : (
+            <ButtonView
+              onPress={() => {
+                setToSellSelected(3);
+              }}>
+              <ButtonText>Aluguel</ButtonText>
+            </ButtonView>
+          )}
         </ButtonContainer>
       </FilterView>
 
       <FilterView>
         <Title>Preço: </Title>
         <PriceView>
-          <PriceInput 
-            placeholder='Mín.' 
-            keyboardType='numeric' 
+          <PriceInput
+            placeholder="Mín."
+            keyboardType="numeric"
             onChange={(text) => setMinPrice(text)}
           />
-          <PriceInput 
-            placeholder='Máx.' 
-            keyboardType='numeric' 
-            onChange={(text) => setMaxPrince(text)}
+          <PriceInput
+            placeholder="Máx."
+            keyboardType="numeric"
+            onChange={(text) => setMaxPrice(text)}
           />
         </PriceView>
       </FilterView>
 
       <FilterView>
-        <Title>Tipo</Title>
-        
-        <ButtonContainer>
-          <ButtonView>
-            <ButtonText>Qualquer</ButtonText>
-          </ButtonView>
-          <ButtonView>
-            <ButtonText>Casa</ButtonText>
-          </ButtonView>  
-        </ButtonContainer>
-
-        <ButtonContainer>
-          <ButtonView>
-            <ButtonText>Apartamento</ButtonText>
-          </ButtonView>
-          <ButtonView>
-            <ButtonText>Kitnet</ButtonText>
-          </ButtonView>  
-        </ButtonContainer>
-      </FilterView>
-
-      <FilterView>
         <Title>Quartos</Title>
-        
+
         <ButtonContainer>
           <ButtonView>
             <ButtonText>Qualquer</ButtonText>
@@ -94,7 +109,7 @@ const Filter = () => {
 
       <FilterView>
         <Title>Banheiros</Title>
-        
+
         <ButtonContainer>
           <ButtonView>
             <ButtonText>Qualquer</ButtonText>
@@ -113,7 +128,6 @@ const Filter = () => {
           </ButtonView>
         </ButtonContainer>
       </FilterView>
-
     </ScrollView>
   );
 };
